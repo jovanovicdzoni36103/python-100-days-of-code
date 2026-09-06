71cda28ce57b852a0e962cc918129131bbf2994e
Nikola <nikola.jovanovic.mef@gmail.com>
2026-09-07 00:59:36 +0200

    Add Day 13 debugging drills

---
+++ day_013/Use a Debugger/task.py
@@ -0,0 +1,14 @@
+import random
+
+
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        new_item += random.randint(1, 3)
+        new_item += item
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
